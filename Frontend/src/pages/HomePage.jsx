b6249980e850ad7a5c117c64bbb1ed168afdda12
David Iyaomolere <davidiyaomolere@example.com>
2025-06-21 18:35:32 +0100

    Ill work on the backend later

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,4 +1,7 @@
-const HomePage=()=>{
-    return <div>HomePage</div>
-}
+import NavBar from "../components/Nav.jsx";
+const HomePage = () => {
+  return <div className="min-h-screen">
+    <NavBar />
+  </div>;
+};
 export default HomePage;
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -13,5 +13,8 @@ export default {
   plugins: [
     daisyui,
   ],
+  daisyui:{
+    themes:["forest"],
+  }
 }
 
